394b6d83e736c4e36ba0362f7868758ac461fbae
tanyinghui <yhtan927@gmail.com>
2021-01-16 17:12:25 +0800

    poisson_distribution.py

---
+++ poisson_distribution.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def poisson_pdf():
+    pass
+
+
+def poisson_cdf():
+    pass
+
+
+def generate_poisson_samples():
+    pass
+
+
+if __name__ == "__main__":
+    lamda = np.array([[0.5], [1.0], [1.5]])
+    pass
